519d026fe026576633273676f61abe0100a1ac23
melpon <shigemasa7watanabe+github@gmail.com>
2024-09-20 10:50:34 +0900

    更新

--- buildbase.py
+++ buildbase.py
@@ -1770,6 +1770,8 @@ class Platform(object):
             self._check(p.arch == "armv8")
         elif p.os in ("ios", "android"):
             self._check(p.arch is None)
+        elif p.os == "ubuntu":
+            self._check(p.arch in ("x86_64", "armv8"))
         else:
             self._check(p.arch in ("x86_64", "arm64", "hololens2"))
 
@@ -1799,7 +1801,8 @@ class Platform(object):
         if target.os == "ubuntu":
             self._check(build.os == "ubuntu")
             self._check(build.arch == "x86_64")
-            self._check(build.osver == target.osver)
+            if target.arch == "x86_64":
+                self._check(build.osver == target.osver)
         if target.os == "raspberry-pi-os":
             self._check(build.os == "ubuntu")
             self._check(build.arch == "x86_64")
